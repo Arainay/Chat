e06f8d014680178385d538f983eef5aba5eb7c8e
arainay <arainay@gmail.com>
2020-03-28 18:12:00 +0800

    Initial

---
+++ client/webpack.config.js
@@ -0,0 +1,120 @@
+const webpack = require('webpack');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const argv = require('yargs').argv;
+
+const outputPath = path.resolve(__dirname, './build');
+const isDevelopment = argv.mode === 'development';
+const isProduction = !isDevelopment;
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/, outputPath],
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                isProduction ? require('cssnano') : () => {},
+                require('autoprefixer')
+              ]
+            }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png)$/,
+        exclude: [/node_modules/, outputPath],
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: outputPath,
+    publicPath: '/',
+    filename: 'bundle.js',
+    pathinfo: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/assets/index.html'),
+      filename: 'index.html',
+      path: outputPath
+    }),
+    new MiniCssExtractPlugin({ filename: 'styles.css' })
+  ],
+  optimization: isProduction ? {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+        extractComments: false
+      })
+    ]
+  } : {},
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      '@app': path.resolve(__dirname, 'src')
+    }
+  },
+  devServer: {
+    contentBase: outputPath,
+    port: 3000,
+    historyApiFallback: true,
+    inline: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    https: false,
+    disableHostCheck: true
+  }
+};
