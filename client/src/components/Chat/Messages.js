c4f303c88c067874b503d2af1c1d7a1f193740e0
Sergey Lobanov <arainay@gmail.com>
2020-04-01 17:12:18 +0300

    Added ScrollToBottom component

--- client/src/components/Chat/Messages.js
+++ client/src/components/Chat/Messages.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+import { useParams } from 'react-router-dom';
 import './messages.scss';
 
-const Messages = ({ messages }) => console.log(messages) || (
-  <section className="messages">
-    {messages.map(item => (
-      <section key={item.id} className="messages__item">{item.text}</section>
-    ))}
-  </section>
-);
+const Messages = ({ messages }) => {
+	const { name } = useParams();
+
+	return (
+		<ScrollToBottom className="messages">
+			{messages.map(item => (
+				<section key={item.id} className="messages__item">
+					<span className="messages__username">{name}: </span>
+					{item.text}
+				</section>
+			))}
+		</ScrollToBottom>
+	);
+};
 
 export default Messages;
