ca7d6a1c6b536f282e32549ad2877b369c659432
arainay <arainay@gmail.com>
2020-04-02 17:35:34 +0800

    Updated view

--- client/src/components/Chat/Messages.js
+++ client/src/components/Chat/Messages.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
 import { useParams } from 'react-router-dom';
+import classnames from 'classnames';
 import './messages.scss';
 
 const Messages = ({ messages }) => {
-	const { name } = useParams();
+  const { name } = useParams();
 
-	return (
-		<ScrollToBottom className="messages">
-			{messages.map(item => (
-				<section key={item.id} className="messages__item">
-					<span className="messages__username">{name}: </span>
-					{item.text}
-				</section>
-			))}
-		</ScrollToBottom>
-	);
+  console.log({name, messages});
+
+  return (
+    <ScrollToBottom className="messages">
+      {messages.map(item => {
+        const isSelf = name.toLowerCase() === item.user.toLowerCase();
+
+        return (
+          <section
+            key={item.id}
+            className={classnames('messages__item', { 'messages__item--self': isSelf })}
+          >
+            <span className={classnames('messages__item-username', { 'messages__item-username--self': isSelf })}>
+              {item.user}
+            </span>
+            <span className={classnames('messages__item-text', { 'messages__item-text--self': isSelf })}>
+              {item.text}
+            </span>
+          </section>
+        );
+      })}
+    </ScrollToBottom>
+  );
 };
 
 export default Messages;
